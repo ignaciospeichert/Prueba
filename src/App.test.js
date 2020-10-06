fdc658c1c66b4a27e1e789d55587124a795abab6
ignacio <ignaciospeichert>
2020-10-06 12:22:18 -0300

    "testeos"

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {
+  render
+} from '@testing-library/react';
 import App from './App';
+import {
+  func
+} from 'prop-types';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const {
+    getByText
+  } = render( < App / > );
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+jugueteloco(2, 3);
+
+var persona = {
+  nombre: "Jorge",
+  edad: 33,
+}
+
+
+function jugueteloco(datoA, datoB) {
+  ;
+  resultado = datoA + datoB
+  return resultado;
+}
